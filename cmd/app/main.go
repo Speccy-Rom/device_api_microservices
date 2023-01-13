89c7ba9fc38b7721311e8194584562aa630a9ef4
speccy-rom <speccy.rom@gmail.com>
2023-01-13 23:09:53 +0300

    device_api_store: add config

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"database/sql"
+	"device_api_microservices/pkg/helpers/pg"
 	"fmt"
 	"github.com/d-kolpakov/logger"
+	"github.com/dhnikolas/configo"
+	_ "github.com/lib/pq"
+	"github.com/pressly/goose"
 )
 
 const ServiceName = "device_api_shops"
@@ -24,4 +29,35 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	cfg := &pg.Config{}
+	cfg.Host = configo.EnvString("db-host", "")
+	cfg.Username = configo.EnvString("db-username", "")
+	cfg.Password = configo.EnvString("db-password", "")
+	cfg.Port = configo.EnvString("db-port", "")
+	cfg.DbName = configo.EnvString("db-name", "")
+	cfg.Timeout = 5
+	config, err := pg.NewPoolConfig(cfg)
+	if err != nil {
+		l.Err(err)
+		panic(err)
+	}
+	config.MaxConns = 100
+	c, err := pg.NewConnection(config)
+	if err != nil {
+		l.Err(err)
+		panic(err)
+	}
+
+	mdb, err := sql.Open("postgres", config.ConnString())
+	if err != nil {
+		l.Err(err)
+		panic(err)
+	}
+	err = goose.Up(mdb, "/var")
+	if err != nil {
+		l.Err(err)
+		panic(err)
+	}
+
 }
